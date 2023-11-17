41efd57139537e03c086b4eaf033126a7bbc2ed6
ktyeon <84512238+ktyeon@users.noreply.github.com>
2023-11-17 14:40:01 -0500

    Time: 18 ms (30.71%), Space: 43.4 MB (27.92%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,22 +14,27 @@
  * }
  */
 class Solution {    
-        public int pathSum(TreeNode root, int targetSum) {
-            // If the root is null, there are no paths
+    public int pathSum(TreeNode root, int targetSum) {
+        
+        // If the root is null, there are no paths
         if (root == null)
+            
             return 0;
 
         // Call the helper method to calculate paths starting from the current node
         // Include paths from the left and right subtrees
         return getSum(root, targetSum, 0) + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+        
     }
 
     // Helper method to calculate paths starting from the current node
     public int getSum(TreeNode root, int targetSum, long sum) {
+        
         int res = 0;
 
         // If the current node is null, return 0
         if (root == null)
+            
             return res;
 
         // Add the current node's value to the running sum
@@ -37,10 +42,12 @@ class Solution {
 
         // If the running sum equals the target sum, increment the result count
         if (sum == targetSum)
+            
             res++;
 
         // Recursively explore paths in the left and right subtrees
         res += getSum(root.left, targetSum, sum);
+        
         res += getSum(root.right, targetSum, sum);
 
         // Backtrack: subtract the current node's value to explore other paths
