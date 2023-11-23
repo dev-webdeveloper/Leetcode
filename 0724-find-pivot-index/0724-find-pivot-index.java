ff468354f029b3bec61b73bcf5f2b586f76dcf09
ktyeon <84512238+ktyeon@users.noreply.github.com>
2023-11-23 12:17:51 -0500

    Time: 1 ms (99.12%), Space: 44.2 MB (33.57%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        
+        int leftSum = 0;
+        
+        for (int num : nums) {
+        
+            totalSum += num;
+        
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+        
+            if (leftSum == totalSum - leftSum - nums[i]) {
+            
+                return i;
+            
+            }
+           
+            leftSum += nums[i];
+        
+        }
+        
+        return -1; // If no pivot index is found
+    }
+}
